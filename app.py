714727333aa89b51786c2d160a95602d40bf4ad4
Pradnya Avhad <avhad.pradnya21@gmail.com>
2025-09-21 11:19:48 +0530

    update app message

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Pradnya. I am Devops Engineer. I have worked on AWS also."
+    return "Hello from Pradnya. I am Devops Engineer. I have worked on AWS also. I have worked on CICD also."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
