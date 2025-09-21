662213233f4e88644aec0aefa2279f01566e23a6
Pradnya Avhad <avhad.pradnya21@gmail.com>
2025-09-21 10:47:29 +0530

    Update app message for CI/CD test

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Pradnya. I am Devops Engineer. "
+    return "Hello from Pradnya. I am Devops Engineer. I have worked on AWS also."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
